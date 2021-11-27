9602ba8d5c26f41b33d70bd61043513bacc50016
rk3776 <rk3776@nyu.edu>
2021-11-27 07:22:25 +0400

    Nov 27

---
+++ app.js
@@ -0,0 +1,8 @@
+var movie = document.querySelector(".video movie");
+var color = document.querySelector(".button color");
+var container = document.querySelector(".container");
+var button = document.getElementById(".button button");
+
+function buttonclick(){
+  button.style.backgroundColor = "green";
+}
